c89b7c718d2757ffb6e79775ba4ac917f6cf901a
REYAN <107745747+REYAN-YADAV@users.noreply.github.com>
2022-12-27 21:51:11 +0530

    commit

--- var_comm_subst.sh
+++ var_comm_subst.sh
@@ -15,10 +15,14 @@ echo "There are two ways we can do command sustitution.
 echo "the today date is : $(date+%D)" # date command used for date and time but we want only time so we use date+%d command
 
 #To print out no.of files in the current directory.
-echo " No.of files in the current directory is : $(ls | wc -l)" # ls =  command used for long listing and wc = command for word counting , (|) = this is known as piping when we want command line argument from previous command out put then we use piping
+echo " No.of files in the current directory is : $(ls | wc -l)" # ls =  command used for long listing and wc = command for word counting , 
+                                                                #(|) = this is known as piping when we want command line argument from previous command 
+                                                                 #       out put then we use piping
 
 #To print current working diectory.
 echo " The current working directory is : $(pwd)" # pwd = present working directory this command will show which directory (folder) i am in right now
 
 #To print no.of lines in current file.
-echo " The no.of lines in current file is : $(cat var_comm_subst.sh | wc -l)" # cat command used for view or edit content of any file. here cat var_comm_subst.sh command will show content of this file and by using piping we count no.of line  
+echo " The no.of lines in current file is : $(cat var_comm_subst.sh | wc -l)" # cat command used for view or edit content of any file. 
+                                                
+                                          #Here cat var_comm_subst.sh command will show content of this file and by using piping we are count no.of line  
