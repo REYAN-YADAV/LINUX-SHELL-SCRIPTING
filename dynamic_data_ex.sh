330a76350935348ca8c703fa733b3f40aae1ba79
REYAN <107745747+REYAN-YADAV@users.noreply.github.com>
2022-12-29 22:49:34 +0530

    commit
    read student data and display to console

---
+++ dynamic_data_ex.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#Write a script to read students data and display to console for conformation.
+read -p "Enter your name : " NAME
+read -p  "Enter your roll number : " ROLLNO
+read -p  "Enter your Marks : " MARKS
+echo "Please confirm your details"
+echo "*****************************"
+echo "Student name : $NAME"
+echo "Student ROLLNO : $ROLLNO"
+echo "Student MARKS : $MARKS"
